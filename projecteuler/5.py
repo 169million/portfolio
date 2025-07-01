95a79e859ae026bb1b0974aa82d22b2f67ee9abe
169million <carsone2026@gmail.com>
2025-06-30 23:24:49 -0700

    Update 5.py

--- projecteuler/5.py
+++ projecteuler/5.py
@@ -1,4 +1,4 @@
 x=20
-while any(x%y for y in range(1,21)): x+=1
+while any(x%y for y in range(1,21)): x+=20
 print(x)
 # x starts at 20 bc that is the highest number in the range, then while if there is any number not divisible by 1 through 20 then add 1 to x if the any fails then the code continues to then printing the final product
